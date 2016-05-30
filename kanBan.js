a965ef9b59a08715a2cafee2badb72cce4ca7a71
Regan <regs.co@gmail.com>
2016-05-29 21:43:47 -0400

    added animation

--- kanBan.js
+++ kanBan.js
@@ -1,51 +1,51 @@
 
-//
-// $(function () {
-//   $('.postits').click(function () {
-//     if($(this).hasClass('to-do')) {
-//       $('.postits').removeClass('to-do')
-//       .addClass('doing');
-//     }
-//
-//   });
-// });
-
-
 $(function() {
-
-  $('#input-field').focus();
+  // add task click
+  // $('#input-field').focus();
   $('#add-button').click(function (e) {
     var newTask = $('#input-field').val();
     var li = $('<div class="postits">' + '<div class="post-it">' + newTask + '</div>')
-    var postIt = li.addClass('.postits');
+    var postIt = li.addClass('.postits animated bounceInDown');
     $('#body1').append(postIt);
     $('#input-field').val('').focus();
-    console.log(newTask);
+
+  });
+  // add task using enter
+  $('#input-field').keyup(function(event) {
+    if (event.keyCode === 13) {
+      $('#add-button').click();
+    }
   });
 
+  /* delete button using mouse */
   $('#delete-button').click(function () {
     $('#input-field').val('');
   });
 
-  $('#boards').on("click", '.postits', function() {
+  // delete button using esc
+  $('#input-field').keyup(function(event) {
+    if (event.keyCode == 27) {
+    $('#delete-button').click();
+    }
+  });
+
+  $('#boards').on('click', '.postits', function() {
     var task = $(this);
     var parentTask = $(task.parent().attr('id'));
     var parentId = parentTask.selector;
 
-    if (parentId === "body1") {
+    if (parentId === 'body1') {
        $(this).remove('.postits');
        $('#body2').append(task);
+       $(this).css('background-color', '#F1C40F');
        //console.log('click');
-      } else if (parentId === "body2") {
+     } else if (parentId === 'body2') {
          $(this).remove('.postits');
          $('#body3').append(task);
+         $(this).css('background-color', '#F39C12');
        //console.log('click');
       } else {
         $(this).remove('.postits');
       }
     });
   });
-
-// add keypress for "+" and "x" buttons. bind to enter and esc
-//center post-it
-//animate
